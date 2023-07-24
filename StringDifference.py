d410c1554a23fd6ad3c446699a981999ba376c54
Chad <dachesh@users.noreply.github.com>
2023-07-24 09:24:18 -0700

    corrected logic to remove indexing error and capture correct list

--- StringDifference.py
+++ StringDifference.py
@@ -4,13 +4,22 @@ import difflib
 
 str1 = "First things first, I'm an artist."
 #print(type(str1))
-str2 = "Second thing first, I can't count because I'm an artist"
+str2 = "Second thing first, I can't count because I'm an artist."
 #print(type(str2))
 
 strset1 = str1.split()
-print(strset1)
+#print(strset1)
 strset2 = str2.split()
-print(strset2)
+#print(strset2)
 
-diff = difflib.Differ(strset1,strset2)
-print(diff)
+wordlist = list(set((strset1 + strset2)))
+uniqlist = []
+
+for i in range(len(wordlist)):
+	item = wordlist[i]
+	if item in strset1 and item in strset2:
+		continue
+	else:
+		uniqlist.append(item)
+
+print(uniqlist)
