cbd4a18043b7860ca751c7e030ce34b7b7a9d9d7
Chad <dachesh@users.noreply.github.com>
2023-07-24 07:40:30 -0700

    added splits to each string

--- StringDifference.py
+++ StringDifference.py
@@ -1,5 +1,16 @@
 # String Difference
 
-str1 = "First things first, I'm an artist."
-str2 = "Second thing first, I can't count because I'm an artist"
+import difflib
 
+str1 = "First things first, I'm an artist."
+#print(type(str1))
+str2 = "Second thing first, I can't count because I'm an artist"
+#print(type(str2))
+
+strset1 = str1.split()
+print(strset1)
+strset2 = str2.split()
+print(strset2)
+
+diff = difflib.Differ(strset1,strset2)
+print(diff)
